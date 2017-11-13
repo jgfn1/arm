dbd5e74bfcefde9bcb0e067fdb39f753655d09f6
jgfn1 <jgfn@cin.ufpe.br>
2017-11-13 03:24:59 -0300

    Added shell commands to install the cross-compilers needand the ARM emulator (QEMU-ARM).

--- arm_compile.sh
+++ arm_compile.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
+#Uncomment the lines below if you still do not have the necessary
+#software to compile, emulate and run the ARM code:
+
+
+# sudo apt-get install libc6-armel-cross libc6-dev-armel-cross
+# sudo apt-get install binutils-arm-linux-gnueabi
+# sudo apt-get install libncurses5-dev
+
 arm-linux-gnueabi-as -o $1.o $1.s
 arm-linux-gnueabi-ld -o $1 $1.o
 ./$1
