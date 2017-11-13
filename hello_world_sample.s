7e7436861be6441220cd4f1fb2ca033c3f25f497
jgfn1 <jgfn@cin.ufpe.br>
2017-11-13 02:10:17 -0300

    Hello world code sample used as first-moments-gui

---
+++ hello_world_sample.s
@@ -0,0 +1,20 @@
+.data
+
+msg: .ascii "Hello, ARM World!\n"
+len = . - msg
+
+.text
+
+.global _start
+_start:
+/* write syscall */
+mov r0, #1 
+ldr r1, =msg 
+ldr r2, =len 
+mov r7, #4 
+svc #0 
+
+/* exit syscall */
+mov r0, #0 
+mov r7, #1 
+svc #0
